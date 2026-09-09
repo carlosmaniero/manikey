38e2bc84df516f2f72ad34b080bb9328e65f1cd7
Carlos Maniero <carlos@maniero.me>
2026-09-09 16:40:45 -0300

    add screw holes to the rj45 adapter
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

---
+++ src/connectors/rj45/cad/adapter_placement.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import sys
+import manifold3d
+from dataclasses import dataclass
+from injector import inject, singleton
+from core.context import injector
+from core.manifold_ext.object import ManifoldObject
+from connectors.rj45.model import RJ45PlacementModel
+
+
+@singleton
+@inject
+@dataclass
+class RJ45AdapterPlacementCAD(ManifoldObject):
+    model: RJ45PlacementModel
+
+    def assemble(self) -> manifold3d.Manifold:
+        adapter = self.deps.stls["build/connectors/rj45/cad/adapter.stl"]
+        return adapter.rotate([0, 180, 0]).translate(
+            self.model.translation_coords
+        )
+
+
+if __name__ == "__main__":
+    adapter_placement = injector.get(RJ45AdapterPlacementCAD)
+    adapter_placement.program(sys.argv)
--- src/connectors/rj45/cad/placement.py
+++ src/connectors/rj45/cad/placement.py
@@ -19,7 +19,24 @@ class RJ45AdapterFrontPlacementCAD(ManifoldObject):
 
         placement = front.translate(self.model.translation_coords)
 
-        return placement ^ body
+        screw_holes = M()
+        for coords in self.model.rj45_model.front_screw_hole_coords:
+            hole = (
+                M.cylinder(
+                    radius_low=self.model.rj45_model.screw_hole_radius,
+                    radius_high=self.model.rj45_model.screw_hole_radius,
+                    height=self.model.rj45_model.screw_hole_height * 4,
+                    center=True,
+                    circular_segments=60,
+                )
+                .rotate([90, 0, 0])
+                .translate(coords)
+            )
+            screw_holes += hole
+
+        screw_holes = screw_holes.translate(self.model.translation_coords)
+
+        return (placement - screw_holes) ^ body
 
 
 if __name__ == "__main__":
